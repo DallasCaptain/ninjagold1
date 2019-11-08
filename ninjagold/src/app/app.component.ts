e8607e81c915c2799cbd8bc1f2c53f061cea952d
Your Name <you@example.com>
2019-11-08 14:11:32 -0600

    html phase 1

--- ninjagold/src/app/app.component.ts
+++ ninjagold/src/app/app.component.ts
@@ -7,4 +7,19 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'ninjagold';
+
+
+  submitform(whichform: any): void { 
+    const actions ={
+      'FARM':[2,4],
+      'CAVE':[5,6],
+      'HOUSE':[7,9],
+      'CASINO':[-100,200]
+    }
+    console.log(Math.floor(Math.random()*(actions[whichform][1]) + actions[whichform][0]))
+    console.log(`Click event is working from form: ${whichform}`);
+}
+
+
+
 }
