14fb9319f775ab26332b17cc5dc48f4408af5c0c
Your Name <you@example.com>
2019-11-13 12:05:50 -0600

    nested routing phase 1

--- ninjagold/src/app/task/task.component.ts
+++ ninjagold/src/app/task/task.component.ts
@@ -9,7 +9,9 @@ export class TaskComponent implements OnInit {
   counter: number = 0;
   quote: string ;
 
-  constructor(private _TaskTalk: TasktalkService) { }
+  constructor(private _TaskTalk: TasktalkService) {
+
+   }
 
   ngOnInit() {
     this._TaskTalk.currentquote.subscribe(quote => this.quote = quote)
